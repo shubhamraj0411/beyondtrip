92111d2af30a4fec8e584e972b58eb686fdf8614
Shubham Raj <YOUR_GITHUB_EMAIL>
2026-09-15 16:26:42 +0530

    Update BeyondTrip code

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ import { apiRouter } from './src/server/api.ts';
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 
